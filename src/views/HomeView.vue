9ce933a02b6430196056f7706dd925e1793f1af3
andyvy <thienvy95@gmail.com>
2022-06-03 10:23:20 +0700

    register section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,12 +11,21 @@
       </div>
     </div>
   </div>
+  <div class="updates">
+    <div class="container">
+      <h2>never miss a post, Register for your free account today!</h2>
+      <RouterLink class="router-button" to="#">
+        Register for FireBlogs <Arrow class="arrow arrow-light" />
+      </RouterLink>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { reactive, ref } from "vue";
 import BlogPost from "@/components/UI/BlogPost.vue";
 import BlogCard from "@/components/UI/BlogCard.vue";
+import Arrow from "@/assets/Icons/arrow-right-light.svg";
 
 export interface ISamplePost {
   title: string;
@@ -67,6 +76,42 @@ const sampleBlogCards: IBlogCard[] = [
     font-weight: 300;
     font-size: 28px;
     margin-bottom: 32px;
+  }
+}
+
+.updates {
+  .container {
+    padding: 100px 24px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+
+      @media (min-width: 800px) {
+        margin-left: auto;
+      }
+    }
+
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+      @media (min-width: 800px) {
+        text-align: initial;
+        font-size: 40px;
+      }
+    }
   }
 }
 </style>
