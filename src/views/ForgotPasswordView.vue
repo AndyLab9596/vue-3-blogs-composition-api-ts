19f967b63e5a6ac652b7c7b388ca365258be4d5e
andyvy <thienvy95@gmail.com>
2022-06-04 07:25:50 +0700

    authentication page in-progress

--- src/views/ForgotPasswordView.vue
+++ src/views/ForgotPasswordView.vue
@@ -1,9 +1,63 @@
 <template>
-  <div>
-    <h3>Forgot Password</h3>
+  <div class="reset-password">
+    <ModalVue
+      v-if="isModalActive"
+      @close-modal="closeModal"
+      :modal-message="modalMessage"
+    />
+    <LoadingVue v-if="isLoading" />
+    <div class="form-wrap">
+      <form class="reset">
+        <p class="login-register">
+          Back to
+          <RouterLink class="router-link" :to="{ name: 'Login' }">Login</RouterLink>
+        </p>
+        <h2>Reset Password</h2>
+        <p>Forgot your password? Enter your email to reset it</p>
+        <div class="inputs">
+          <div class="input">
+            <input type="text" placeholder="Email" v-model="email" />
+            <EmailIcon class="icon" />
+          </div>
+        </div>
+        <button>Reset</button>
+        <div class="angle"></div>
+      </form>
+      <div class="background"></div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import EmailIcon from "@/assets/Icons/envelope-regular.svg";
 
-<style scoped></style>
+import { ref } from "vue";
+import ModalVue from "@/components/UI/ModalVue.vue";
+import LoadingVue from "@/components/UI/LoadingVue.vue";
+
+const email = ref<string>("");
+const isModalActive = ref<boolean>(false);
+const isLoading = ref<boolean>(false);
+const modalMessage = ref<string>("");
+
+const closeModal = () => {
+  isModalActive.value = false;
+};
+</script>
+
+<style lang="scss" scoped>
+.reset-password {
+  position: relative;
+  .form-wrap {
+    .reset {
+      h2 {
+        margin-bottom: 8px;
+      }
+      p {
+        text-align: center;
+        margin-bottom: 32px;
+      }
+    }
+  }
+}
+</style>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,9 +1,58 @@
 <template>
-  <div>
-    <h3>Register View</h3>
+  <div class="form-wrap">
+    <form class="register">
+      <p class="login-register">
+        Already have an account
+        <RouterLink class="router-link" :to="{ name: 'Login' }">Login</RouterLink>
+      </p>
+      <h2>Create your FireBlog Account</h2>
+      <div class="inputs">
+        <div class="input">
+          <input type="text" placeholder="First Name" v-model="firstName" />
+          <UserIcon class="icon" />
+        </div>
+        <div class="input">
+          <input type="text" placeholder="Last Name" v-model="lastName" />
+          <UserIcon class="icon" />
+        </div>
+        <div class="input">
+          <input type="text" placeholder="Username" v-model="username" />
+          <UserIcon class="icon" />
+        </div>
+        <div class="input">
+          <input type="text" placeholder="Email" v-model="email" />
+          <EmailIcon class="icon" />
+        </div>
+        <div class="input">
+          <input type="password" placeholder="Password" v-model="password" />
+          <PasswordIcon class="icon" />
+        </div>
+      </div>
+      <button>Sign Up</button>
+      <div class="angle"></div>
+    </form>
+    <div class="background"></div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import EmailIcon from "@/assets/Icons/envelope-regular.svg";
+import PasswordIcon from "@/assets/Icons/lock-alt-solid.svg";
+import UserIcon from "@/assets/Icons/user-alt-light.svg";
 
-<style scoped lang="scss"></style>
+import { ref } from "vue";
+
+const email = ref<string>("");
+const password = ref<string>("");
+const firstName = ref<string>("");
+const lastName = ref<string>("");
+const username = ref<string>("");
+</script>
+
+<style scoped lang="scss">
+.register {
+  h2 {
+    max-width: 350px;
+  }
+}
+</style>
